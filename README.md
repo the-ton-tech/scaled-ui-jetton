ef0fadff0e958c8065922e6ba07f51d6a0888ec8
krigga <krigga7@gmail.com>
2025-10-22 13:09:33 +0300

    chore: update dependencies, deployment, readme

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ Based on the [Jetton with Governance](https://github.com/ton-blockchain/stableco
 ## Differences from the base project
 
 This implementation differs from the base project in that it implements the Scaled UI TEP (specifically, the `get_display_multiplier` get method, as well as a `display_multiplier_changed#ac392598` event when the return values of that get method change), and also another feature to help with the administration of the contract, particularly, the ability to schedule a change of the display multiplier at a certain timestamp, as well as a permissionless operation to enact said change when the time comes.
+
+The deployment script is changed to:
+1) Use USDT wallet code (without compilation; builds library cell)
+2) Emit a `display_multiplier_changed` upon initialization in accordance with the TEP
--- scripts/deployJettonMinter.ts
+++ scripts/deployJettonMinter.ts
@@ -1,66 +1,31 @@
-import {Cell, toNano} from '@ton/core';
+import {beginCell, Cell, toNano} from '@ton/core';
 import {JettonMinter} from '../wrappers/JettonMinter';
 import {compile, NetworkProvider} from '@ton/blueprint';
-import {jettonWalletCodeFromLibrary, promptBool, promptUrl, promptUserFriendlyAddress} from "../wrappers/ui-utils";
-import { Librarian } from '../wrappers/Librarian';
+import {promptUrl} from "../wrappers/ui-utils";
 
 export async function run(provider: NetworkProvider) {
-    const isTestnet = provider.network() !== 'mainnet';
-
     const ui = provider.ui();
-    const jettonWalletCodeRaw = await compile('JettonWallet');
 
-    const adminAddress = await promptUserFriendlyAddress("Enter the address of the jetton owner (admin):", ui, isTestnet);
+    const adminAddress = provider.sender().address;
 
-    // e.g "https://bridge.ton.org/token/1/0x111111111117dC0aa78b770fA6A738034120C302.json"
-    const jettonMetadataUri = await promptUrl("Enter jetton metadata uri (https://jettonowner.com/jetton.json)", ui)
-
-    const jettonWalletCode = jettonWalletCodeFromLibrary(jettonWalletCodeRaw);
-
-    const librarianCode = await compile('Librarian');
-    const librarian = provider.open(Librarian.createFromConfig({code: jettonWalletCodeRaw}, librarianCode));
-
-    const librarianState = await librarian.getState();
-
-    let librarianFound  = false;
-    let libraryDeployed = false;
-
-    let emptyCell = new Cell();
-
-    if(librarianState.state.type == 'active') {
-        librarianFound = true;
-        if(librarianState.state.code && librarianState.state.data) {
-            const codeCell = Cell.fromBoc(librarianState.state.code)[0];
-            const dataCell = Cell.fromBoc(librarianState.state.data)[0];
-            if(codeCell.equals(emptyCell) && dataCell.equals(emptyCell)) {
-                libraryDeployed = true;
-            }
-        }
+    if (adminAddress === undefined) {
+        throw new Error('Admin address is not set');
     }
 
-    if(!(librarianFound && libraryDeployed)) {
+    // USDT wallet code
+    const jettonWalletCode = new Cell({
+        exotic: true,
+        bits: beginCell().storeUint(2, 8).storeBuffer(Buffer.from('j0Utek39dAZraCNlF3JZ7QVzRDW+drX9S9XYryt8PWg=', 'base64')).endCell().bits,
+    });
 
-        ui.write("DANGER ZONE!");
-        if(!librarianFound) {
-            ui.write("Librarian contract is not found, run deployLibrary first.")
-        } else {
-            ui.write("Librarian contract present, but it's state indicates that the library is not yet deployed.");
-            ui.write("Re-run deployLibrary but increase the TON amount or adjust librarian DEFAULT_DURATION.");
-        }
-
-        if(!await promptBool("Can you guarantee that library is already deployed?", ["Yes", "No"], ui, false)) {
-            return -1;
-        }
-    } else {
-        ui.write(`Librarian found at ${librarian.address.toString()}`);
-    }
+    const jettonMetadataUri = await promptUrl("Enter jetton metadata uri (https://example.com/jetton.json)", ui)
 
     const minter = provider.open(JettonMinter.createFromConfig({
-            admin: adminAddress.address,
+            admin: adminAddress,
             wallet_code: jettonWalletCode,
             jetton_content: {uri: jettonMetadataUri}
         },
         await compile('JettonMinter')));
 
-    await minter.sendDeploy(provider.sender(), toNano("1.5")); // send 1.5 TON
+    await minter.sendSetScaledUiData(provider.sender(), 1n, 1n, "initialization", false, toNano('1.5'));
 }
